744d8245bb96f959a5f72ea74a6d266b71057eb6
Magesh <tkmagesh77@gmail.com>
2017-08-18 10:30:36 +0530

    memoize - Part - 1

--- 02-Closures/isPrime.js
+++ 02-Closures/isPrime.js
@@ -42,3 +42,30 @@ var isPrime = (function(){
         return cache[n];
     }
 })();
+
+var isOddOrEven = (function(){
+    var cache = {};
+    function checkOddOrEven(n){
+        console.log('Processing ', n);
+        return n % 2 === 0 ? 'even' : 'odd';
+    }
+    return function (n){
+        if (typeof cache[n] === 'undefined')
+            cache[n] = checkOddOrEven(n);
+        return cache[n];
+    }
+})();
+
+function memoize(fn){
+    var cache = {};
+    return function (n){
+        if (typeof cache[n] === 'undefined')
+            cache[n] = fn(n);
+        return cache[n];
+    }
+}
+
+var isOddOrEven = memoize(function(n){
+    console.log('processing ', n);
+    return n % 2 === 0 ? 'even' : 'odd';
+});
