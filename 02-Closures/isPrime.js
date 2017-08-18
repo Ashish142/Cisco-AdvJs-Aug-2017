00f374f2d641878cc64861a383fd3103282ea5c0
Magesh <tkmagesh77@gmail.com>
2017-08-18 10:10:33 +0530

    isPrime

---
+++ 02-Closures/isPrime.js
@@ -0,0 +1,44 @@
+var isPrime = (function(){
+    var cache = {};
+    function isPrime(n){
+        if (typeof cache[n] !== 'undefined')
+            return cache[n];
+
+        console.log('processing ', n);
+        if (n <= 3){
+            cache[n] = true;
+            return cache[n];
+        }
+        cache[n] = true;
+        for(var index = 2; index <= (n/2); index++){
+            if (n % index === 0){
+                cache[n] = false;
+                break;
+            }
+        }
+        return cache[n];
+    }
+	return isPrime;
+})();
+
+var isPrime = (function(){
+    var cache = {};
+    function checkPrime(n){
+        console.log('Processing ', n);
+        if (n <= 3){
+            return true;
+        }
+        
+        for(var index = 2; index <= (n/2); index++){
+            if (n % index === 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    return function (n){
+        if (typeof cache[n] === 'undefined')
+            cache[n] = checkPrime(n);
+        return cache[n];
+    }
+})();
